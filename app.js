963c1b975d5c75f6b1654a918afcf8ce21dbeff0
hmswrth <mudra.hemanth@gmail.com>
2020-05-16 12:36:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function displayTime() {
+    let date = new Date();
+    //let time = date.toLocaleString('en-US' , {hour : 'numeric', minute : 'numeric', second : 'numeric', hour12 : true } );
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = (hh>=12)? 'PM' : 'AM';
+    if(hh==0){
+        hh=12;
+        session='AM';
+    }
+    hh = checkFormat(hh);
+    mm = checkFormat(mm);
+    ss = checkFormat(ss);
+    document.getElementById("clock").innerHTML = hh + ":" + mm + ":" + ss +" "+session;
+   // document.getElementById("clock").innerHTML = time;
+
+    setTimeout(displayTime,0);
+}
+
+function checkFormat(s) {
+    let k = s < 10 ? s = "0" + s : s;
+    return k
+}
+
+displayTime();
