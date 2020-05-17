499ba1fbeb4ef800ddff45fa088935cb045a2e0d
hmswrth <mudra.hemanth@gmail.com>
2020-05-17 15:37:27 +0530

    final commit

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ function displayTime() {
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
-    let session = (hh>=12)? 'PM' : 'AM';
-    if(hh==0){
-        hh=12;
-        session='AM';
+    let session = (hh >= 12) ? 'PM' : 'AM';
+    if (hh == 0) {
+        hh = 12;
     }
+    hh = hh > 12 ? hh - 12 : hh;
     hh = checkFormat(hh);
     mm = checkFormat(mm);
     ss = checkFormat(ss);
-    document.getElementById("clock").innerHTML = hh + ":" + mm + ":" + ss +" "+session;
-   // document.getElementById("clock").innerHTML = time;
+    document.getElementById("clock").innerHTML = hh + ":" + mm + ":" + ss + " " + session;
+    // document.getElementById("clock").innerHTML = time;
 
-    setTimeout(displayTime,0);
+    setTimeout(displayTime, 0);
 }
 
 function checkFormat(s) {
